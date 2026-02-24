3560035a025ddd4cc66e3cd5f58e534f66f9b372
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-02-24 19:12:17 -0300

    ex_1 solved
    Exercise 1 working sucessfully

--- ex_1/src/app/home/home.page.ts
+++ ex_1/src/app/home/home.page.ts
@@ -4,10 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class HomePage {
 
+  temperatura = '';
+  escolha = '';
+  resultado = '';
   constructor() {}
+
+  temp(){
+    const valor = parseFloat(this.temperatura);
+
+    if(isNaN(valor)){
+      this.resultado = 'Por favor, digite um número válido.';
+      return;
+    }
+    else if(this.escolha === 'celsius'){
+      const celsius_fahrenheit = ((valor*9)+160)/5
+      this.resultado = `Resultado: ${celsius_fahrenheit.toFixed(2)} ⁰F`
+    }
+    else if(this.escolha === 'fahrenheit'){
+      const fahrenheit_celsius = (valor-32)*(5/9)
+      this.resultado = `Resultado: ${fahrenheit_celsius.toFixed(2)} ⁰C`
+    }
+  }
+
 
 }
