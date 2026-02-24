ef35a36b2a96920bad110a87b974adc92007b931
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-02-24 20:30:00 -0300

    ex_2 solved
    Exercise 2 working successfully

--- ex_2/src/app/home/home.page.ts
+++ ex_2/src/app/home/home.page.ts
@@ -7,7 +7,36 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
+  resultado = '';
   constructor() {}
+  a = '';
+  b = '';
+  c = '';
+
+  figura(){
+    const x = parseFloat(this.a);
+    const y = parseFloat(this.b);
+    const z = parseFloat(this.c);
+
+    if(isNaN(x) || isNaN(y) || isNaN(z)){
+      this.resultado = `Por favor, insira os 3 valores em seus respectivos campos.`;
+      return;
+    }
+
+    if(x + y > z && x + z > y && y + z > x){
+      if(x === y && y === z){
+        this.resultado = `A figura formada é um triângulo equillátero!`;
+      }
+      else if(x === y || x === z || y === z){
+        this.resultado = `A figura formada é um triângulo isósceles!`;
+      }
+      else{
+        this.resultado = `A figura formada é um triângulo escaleno!`;
+      }
+    }
+    else{
+      this.resultado = `A figura formada não é um triângulo.`
+    }
+  }
 
 }
